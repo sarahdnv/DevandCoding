3c2bdcce40ccebf795706d8eca135d8606aaab49
TaisonEady <taison.eady@gmail.com>
2014-04-02 23:17:40 +1100

    version 1
    Generated basic android project code
    First draft of object classes created
    Started building log activity

---
+++ timeKeeper/src/com/devcode/timekeeper/data/tagItem.java
@@ -0,0 +1,15 @@
+package com.devcode.timekeeper.data;
+
+public class tagItem {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
